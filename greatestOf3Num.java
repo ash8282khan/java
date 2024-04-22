c9903b88b4890d21158c89b021f5565cb76ddea6
ash8282khan <ash8282khan@gmail.com>
2024-04-22 16:30:18 +0530

    day1

---
+++ greatestOf3Num.java
@@ -0,0 +1,20 @@
+//find greatest out of 3 numbers 
+
+public class greatestOf3Num{
+	public static void main(String args[]){
+	int a=10,b=40,c=30;
+		
+		System.out.println("There are 3 numbers (A,B,C)");
+		
+
+if(a>b && a>c)
+System.out.println("Greatest No. is A="+a);
+if(b>c && b>a)
+System.out.println("Greatest No. is B="+b);
+if(c>b && c>a)
+System.out.println("Greatest No. is C="+c);
+
+	
+
+	}
+} 
